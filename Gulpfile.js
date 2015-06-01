4d95ea90b8afb24ddead7d6dca0e39d4d245e57b
Zura Gabievi <zura.gabievi@gmail.com>
2015-06-01 14:31:22 +0400

    Update Gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,19 +1,25 @@
+// gulp modules
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
+// configuration
 var config = require('./config');
 
+// NODE_ENV
 var ENV = process.env.ENV || config.environment;
 var EXT = process.env.EXT || config.extension;
 
+// clean task
 gulp.task('js:clean', function() {
 	gulp.src('./dist/assets/js/*', {read: false}).pipe($.clean());
 });
 
+// copy task
 gulp.task('js:copy', function() {
 	gulp.src('./bower_components/crafty/dist/crafty.*').pipe(gulp.dest('./dist/assets/js'));
 });
 
+// build task
 gulp.task('js:build', ['js:clean', 'js:copy'], function() {
 	return gulp.src('./src/assets/js/*.' + EXT.trim())
 		.pipe($.plumber())
@@ -31,8 +37,10 @@ gulp.task('js:build', ['js:clean', 'js:copy'], function() {
 		.pipe(gulp.dest('./dist/assets/js'));
 });
 
+// watch task
 gulp.task('js:watch', ['js:build'], function() {
 	gulp.watch('./src/assets/js/**/*.' + EXT.trim(), ['js:build']);
 });
 
+// default task
 gulp.task('js', ['js:watch']);
